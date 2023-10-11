3157614bcfe1760d329a04a1afc299927aa06c2e
Wishmi Tharuka <92284830+vishmitharuka57@users.noreply.github.com>
2023-10-11 09:15:34 +0530

    first request called

--- 01_HelloServelet/src/HelloServlet.java
+++ 01_HelloServelet/src/HelloServlet.java
@@ -1,5 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name="MyServlet",urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request ekak awa");
+    }
 }
