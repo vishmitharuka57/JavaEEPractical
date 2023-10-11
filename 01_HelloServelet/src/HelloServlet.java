8235f33e8562375714224276680275aced8d51d2
Wishmi Tharuka <92284830+vishmitharuka57@users.noreply.github.com>
2023-10-11 10:34:30 +0530

    request send and then print "response sended" in browser

--- 01_HelloServelet/src/HelloServlet.java
+++ 01_HelloServelet/src/HelloServlet.java
@@ -4,11 +4,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name="MyServlet",urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Request ekak awa");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Sended");
     }
 }
