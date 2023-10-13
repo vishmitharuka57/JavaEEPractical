b8513c26074f3f0f3569496da064915e151fa9ad
Wishmi Tharuka <92284830+vishmitharuka57@users.noreply.github.com>
2023-10-13 11:41:28 +0530

    new module and content updated

---
+++ 02_HttpRequest/src/MyServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hello")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Application 2 response generated");
+    }
+}
